5b020175886128d2b89c1150c28aa3d3c28c4f4d
Noah <nlk34@zips.uakron.edu>
2021-01-26 11:10:36 -0500

    Remove unneeded declarations

--- stockPrediction.py
+++ stockPrediction.py
@@ -1,17 +1,19 @@
 
 import numpy as np 
 from sklearn.linear_model import LinearRegression
-from sklearn.svm import SVR
 from sklearn.model_selection import train_test_split
 from pandas_datareader import data
-import matplotlib.pyplot as plt
-import pandas as pd
+from datetime import date
+today = date.today()
 
 start_date = '2010-01-01'
-end_date = '2021-1-26'
+end_date = today.strftime("%Y-%m-%d")
 
-df = data.DataReader('AAPL', 'yahoo', start_date, end_date)
+ticker = input("Please enter a ticker ")
+ticker = ticker.upper()
+df = data.DataReader(ticker, 'yahoo', start_date, end_date)
 df = df[['Close']] 
+#print(df.tail())
 
 forecast_out = 30 #'n=30' days
 #Create another column (the target ) shifted 'n' units up
@@ -36,10 +38,8 @@ print("lr confidence = ", lr_confidence)
 
 #set forcast = tp last 30 rows of the original adj close
 x_forecast = np.array(df.drop(['Prediction'],1))[-forecast_out:]
-#print (x_forcast)
+
 
 lr_prediction = lr.predict(x_forecast )
-#svm_prediction = svr_rbf.predict(x_forecast )
 
 print(lr_prediction)
-#print(svm_prediction)
